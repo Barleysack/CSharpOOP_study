164d9cc9ee26d1177f317beb202c92bced43a918
barleysack <hooker4020@naver.com>
2021-05-06 14:48:53 +0900

    Day7_props-to-arrays

---
+++ Day7_Classandinterface/ArrayTestApp/MainApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ArrayTestApp
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+
+
+
+
+        {
+
+
+            Console.WriteLine("array 클래스 사용");
+            int[] array = new int[]{ 5, 2, 1, 5, 6, 7, 8, 9, 2 };
+
+            System.Array.Sort(array);
+            Array.Resize<int>(ref array, 11);
+            
+            Console.WriteLine($"");
+            
+            /*Console.WriteLine($"수학학원 점수");
+            int[] scores = { 3, 4, 5, 6, 7, 8, 9, 10 };//최종 제일 짧은 초기화 방법 
+            *//*scores[0] = 80;
+            scores[1] = 77;
+            scores[2] = 56;
+            scores[3] = 78;
+            scores[4] = 56;*//*
+            */
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum = sum + array[i];
+
+            }
+            float average = (float)sum / array.Length;
+            Console.WriteLine($"수학학원 점수 총합 :{sum}, 평균: {average}");
+        }   
+    }
+}
+
---
+++ Day7_Classandinterface/Day7_01/AbstractBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_AbstractclasstestApp
+{
+    abstract class AbstractBase
+    {
+       protected void ProtectedMethodA()
+        {
+            Console.WriteLine("AbstractBase.ProtectedMethod() 실행");
+
+        }
+
+       public void PublicMeth()
+        {
+            Console.WriteLine("AbstractBase.PublicdMethod() 실행");
+        }
+
+        public abstract void AbstractMethod();//인터페이스 선언과 동일 상속받으면 구현해야함.
+
+    }
+}
