f58549aaa79c8d3c16476ee5388fcce06ce8a976
barleysack <hooker4020@naver.com>
2021-05-06 15:09:19 +0900

    Relocated

---
+++ Day7/Day7_Collection_Indexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day7_Collection_Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
