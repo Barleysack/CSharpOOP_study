45e7065adb6fe87aa5647830d07db9b84fee3d68
barleysack <hooker4020@naver.com>
2021-05-07 12:48:03 +0900

    Exceptions

--- OOP/Day8_ExceptionTestApp/MainApp.cs
+++ OOP/Day8_ExceptionTestApp/MainApp.cs
@@ -10,19 +10,24 @@ namespace Day8_ExceptionTestApp
             int[] array = new int[5];
             try
             {
+                string message = null;
+                int k = message.Length;
                 for (int i = 0; i <= 5; i++)
                 {
                     array[i] = i + 1;//
 
                 }
             }
-            catch (Exception ex)
+            catch (IndexOutOfRangeException ex)
             {
 
                 Console.WriteLine($"배열 크기에서 벗어난 듯 합니다.{ex.Message}");
 
             }
-            
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine($"예외발생:{ex.Message}");
+            }
             Console.WriteLine("다른뭐시기...");
             Console.WriteLine("정상종료");
         }
