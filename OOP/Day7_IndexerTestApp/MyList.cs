824c298de3f1edb5a3ba8e5b4af9b088957cd1c0
barleysack <hooker4020@naver.com>
2021-05-07 12:21:05 +0900

    Arranged Folder Name

--- OOP/Day7_IndexerTestApp/MyList.cs
+++ OOP/Day7_IndexerTestApp/MyList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,11 @@ using System.Threading.Tasks;
 
 namespace Day7_IndexerTestApp
 {
-    class MyList
+    class MyList: IEnumerable, IEnumerator
     {
         private int[] array;
+        private int position = -1;
+
         public int this[int index]//this+꺾쇠- 인덱서! //MyList[i] array의 i번째 값을 가져오겠다
         {
             get
@@ -32,9 +35,32 @@ namespace Day7_IndexerTestApp
         {
             get { return array.Length; }
         }
+
+        public object Current
+        {
+            get { return array[position]; }///현재값 foreach 에만 필요.
+        }
+
+
         public MyList()
         {
             array = new int[3];
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            position = position + 1;
+            return (position < array.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;//초기화
         }
     }
 }
---
+++ OOP/Day8_ExceptionTestApp/MainApp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Day8_ExceptionTestApp
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("프로그램시작!");
+            int[] array = new int[5];
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    array[i] = i + 1;//
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"배열 크기에서 벗어난 듯 합니다.{ex.Message}");
+
+            }
+            
+            Console.WriteLine("다른뭐시기...");
+            Console.WriteLine("정상종료");
+        }
+    }
+}
