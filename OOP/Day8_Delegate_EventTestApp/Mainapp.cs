e0a81064b7c889e2e0cc75caacef15437eae30af
barleysack <hooker4020@naver.com>
2021-05-10 12:47:46 +0900

    Need review-indexer, delegate Today I learned Lambda,Linq,Reflection:)

--- OOP/Day8_Delegate_EventTestApp/Mainapp.cs
+++ OOP/Day8_Delegate_EventTestApp/Mainapp.cs
@@ -2,19 +2,24 @@
  
 namespace Day8_Delegate_EventTestApp//Event를 만들기 위해 ...!
 
-{
+{ delegate int Calcy(int a, int b);
     class Mainapp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Console.WriteLine("Hello World!");
             Calc calc = new();//일반호출
             CalcDELE CB;
             CB = new CalcDELE(calc.Plus);
             Console.WriteLine($"3+5 ={ CB(3,5)}");//동적인 처리를 위한...
             //
             CB = new CalcDELE(calc.Mult);
-            Console.WriteLine($"4*5={CB(4, 5)}");
+            Console.WriteLine($"4*5={CB(4, 5)}");*/
+
+            Calcy calc;
+            calc = (a, b) => a + b;
+            Console.WriteLine(calc(2, 5));
+
         }
     }
 }
---
+++ OOP/Day9_ReflectionTestApp/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+
+namespace Day9_ReflectionTestApp
+{   class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public void PrintProfile() { Console.WriteLine($"{Age},{Name}"); }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Person a = new();
+            Type type = a.GetType();
+            FieldInfo[] fields = type.GetFields();
+            foreach (var item in fields)
+            {
+                Console.WriteLine($"Type:{item.FieldType.Name},Name:{item.Name}");
+
+            }
+            Console.WriteLine("Method 타입 리스트 :");
+            var methods = type.GetMethods();
+            foreach (var item in methods)
+            {
+                Console.WriteLine($"{item.ReturnType.Name},Name:{item.Name}");
+            }
+            Console.WriteLine("Property 타입 리스트 : ");
+            PropertyInfo[] ppt = type.GetProperties();
+            foreach (var item in ppt)
+            {
+                Console.WriteLine($"Type:{item.PropertyType.Name},Name:{item.Name}");
+
+            }
+        }
+    }
+}
