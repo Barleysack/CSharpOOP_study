d16c452f50799236576745b8fda8f08954c1db02
barleysack <hooker4020@naver.com>
2021-05-10 16:12:39 +0900

    files

---
+++ OOP/AttributeTestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AttributeTestApp
+{
+
+    class Myclass
+    {
+        [Obsolete("이 메서드는 폐기되었습니다")]
+        public void OldMethod()
+        {
+            Console.WriteLine("저거 어떻게 만들지");
+
+        }
+        public void NewMethod()
+        {
+            Console.WriteLine("New thang");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass mc = new();  
+            mc.OldMethod();
+
+        }
+    }
+}
